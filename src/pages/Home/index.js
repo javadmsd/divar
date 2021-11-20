967b58ee6dfeeb78aeabffba083f0b8398c097ac
Mohammad javad Masoudian <masoudian.dev@gmail.com>
2021-11-20 18:37:38 +0330

    init

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+import Header from "../../components/Header";
+
+function Home() {
+  return (
+    <div className="container">
+      <Header />
+
+      <div className="banner"></div>
+    </div>
+  );
+}
+
+export default Home;
